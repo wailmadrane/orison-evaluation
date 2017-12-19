194844f8e5721e4c083d214055a7625641c98fd7
Wail Madrane <wail.madrane@gmail.com>
2017-12-19 16:01:05 +0100

    version05

--- plugin/scroll-reveal.js
+++ plugin/scroll-reveal.js
@@ -1,1 +1,20 @@
 
+/*-- plug-in --*/
+
+var $scrollReveal = $('[data-scroll-reveal="true"]');
+var $document = $(document);
+var scrollRevealOffset;
+var num = 0;
+
+$scrollReveal.addClass('hidden');
+
+$document.on("scroll", function(){
+
+    $scrollReveal.each(function() {
+    scrollRevealOffset = $(this).offset().top/3;
+
+        if( $document.scrollTop() > scrollRevealOffset && $(this).hasClass('hidden') ){
+            $(this).removeClass('hidden');
+        }
+    });
+});
